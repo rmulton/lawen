fda6818fe01307e07e069bc57a6934f1cc89d2ef
Joseph Sayegh <sayegh_j@hotmail.com>
2017-10-17 16:41:06 +0200

    Google API and get possibilities

--- webservice_caller/GoogleAPI.py
+++ webservice_caller/GoogleAPI.py
@@ -5,15 +5,18 @@ from model.Transport.Walk import Walk
 from model.Transport.PublicTransport import PublicTransport
 
 class GoogleAPICaller:
+    
+    url = 'https://maps.googleapis.com/maps/api/directions/json?'
+    key = 'AIzaSyC2hKozMP10NcIQmqCPesMX0d5nb0lW6cI'
+
     def __init__ (self, request):
         '''
         Create the different parameters that we will need for the API url
         '''
-        self.url = 'https://maps.googleapis.com/maps/api/directions/json?'
         self.origin = request.request_from
         self.destination = request.request_to
         self.modes = ['driving','walking','bicycling','transit']
-        self.key = 'AIzaSyC2hKozMP10NcIQmqCPesMX0d5nb0lW6cI'
+        
         
     
     def get_possibilities(self):    
@@ -23,7 +26,7 @@ class GoogleAPICaller:
         '''
         possibilities = []
         for mode in self.modes:
-            url_final = self.url + "origin=" + ",".join(str (e) for e in self.origin) + "&destination=" + ",".join(str(f) for f in self.destination) + "&mode=" + mode + "&key=" + self.key
+            url_final = GoogleAPICaller.url + "origin=" + ",".join(str (e) for e in self.origin) + "&destination=" + ",".join(str(f) for f in self.destination) + "&mode=" + mode + "&key=" + GoogleAPICaller.key
             response = requests.get(url_final)
             dico = json.loads(response.content)          
             travel_time = dico["routes"][0]["legs"][0]["duration"]["value"]
@@ -33,6 +36,8 @@ class GoogleAPICaller:
                 transport = PublicTransport(travel_time)
             else:
                 continue         
-            possibilities.append(transport)    
+            possibilities.append(transport)   
         return(possibilities)
+
+
                 
