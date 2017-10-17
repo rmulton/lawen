02f03da6b959bde122cd07acb21f4dc910bb9495
Joseph Sayegh <sayegh_j@hotmail.com>
2017-10-17 15:54:47 +0200

    Google API maps search and function to get possibilities

--- webservice_caller/GoogleAPI.py
+++ webservice_caller/GoogleAPI.py
@@ -1,27 +1,39 @@
 import json
 import requests
 import re
-from model.Request import Request
+from model.Transport.Walk import Walk
+from model.Transport.PublicTransport import PublicTransport
 
 class GoogleAPICaller:
     def __init__ (self,url,request):
+        '''
+        Create the different parameters that we will need for the API url
+        '''
         self.url = url
         #inital_google_url = 'https://maps.googleapis.com/maps/api/directions/json?'
         self.origin = request.request_from
-        #origin = 'Paris'
         self.destination = request.request_to
-        #destination = 'Antony'
-        self.mode = ['driving','walking','bicycling','transit']
+        self.modes = ['driving','walking','bicycling','transit']
         self.key = 'AIzaSyC2hKozMP10NcIQmqCPesMX0d5nb0lW6cI'
         
     
-    def get_transit_times(self):    
-        l = {}
-        for k in self.mode:
-            url_final = self.url + "origin=" + self.origin + "&destination=" + self.destination + "&mode=" + k + "&key=" + self.key
-            text = requests.get(url_final)
-            dico = json.loads(text.content)          
-            l[k] = dico["routes"][0]["legs"][0]["duration"]["value"]
-        print(l)
-
-    #def get_possibilities(self):
+    def get_possibilities(self):    
+        '''
+        Get the different times related to the travel modes and returns 
+        a list of objects corresponding to each travel mode'
+        '''
+        possibilities = []
+        for mode in self.modes:
+            url_final = self.url + "origin=" + ",".join(str (e) for e in self.origin) + "&destination=" + ",".join(str(f) for f in self.destination) + "&mode=" + mode + "&key=" + self.key
+            response = requests.get(url_final)
+            dico = json.loads(response.content)          
+            travel_time = dico["routes"][0]["legs"][0]["duration"]["value"]
+            if mode == 'walking':
+                transport = Walk(travel_time)
+            elif mode == 'transit':
+                transport = PublicTransport(travel_time)
+            else:
+                continue         
+            possibilities.append(transport)    
+        return(possibilities)
+                
