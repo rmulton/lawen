f9185d8de2872c4cec851f6269167d4cb7827ddb
Joseph Sayegh <sayegh_j@hotmail.com>
2017-10-17 15:55:06 +0200

    test function

---
+++ test.py
@@ -0,0 +1,6 @@
+from model.Request import Request
+from webservice_caller.GoogleAPI import GoogleAPICaller
+
+request = Request([48.8809481,2.3568375],[48.7549066,2.3010135])
+caller = GoogleAPICaller('https://maps.googleapis.com/maps/api/directions/json?',request)
+caller.get_possibilities()
