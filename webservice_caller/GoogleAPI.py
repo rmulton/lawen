737950258b393d005531408645a2caf255a62f55
Joseph Sayegh <sayegh_j@hotmail.com>
2017-10-20 10:38:35 +0200

    first exceptions added

--- webservice_caller/GoogleAPI.py
+++ webservice_caller/GoogleAPI.py
@@ -25,19 +25,25 @@ class GoogleAPICaller:
         a list of objects corresponding to each travel mode'
         '''
         possibilities = []
-        for mode in self.modes:
-            url_final = GoogleAPICaller.url + "origin=" + ",".join(str (e) for e in self.origin) + "&destination=" + ",".join(str(f) for f in self.destination) + "&mode=" + mode + "&key=" + GoogleAPICaller.key
-            response = requests.get(url_final)
-            dico = json.loads(response.content)          
-            travel_time = dico["routes"][0]["legs"][0]["duration"]["value"]
-            if mode == 'walking':
-                transport = Walk(travel_time)
-            elif mode == 'transit':
-                transport = PublicTransport(travel_time)
-            else:
-                continue         
-            possibilities.append(transport)   
-        return(possibilities)
+        try:
+            for mode in self.modes:
+                url_final = GoogleAPICaller.url + "origin=" + ",".join(str (e) for e in self.origin) + "&destination=" + ",".join(str(f) for f in self.destination) + "&mode=" + mode + "&key=" + GoogleAPICaller.key
+                response = requests.get(url_final)
+                dico = json.loads(response.content)  
+                travel_time = dico["routes"][0]["legs"][0]["duration"]["value"]
+                if mode == 'walking':
+                    transport = Walk(travel_time)
+                elif mode == 'transit':
+                    transport = PublicTransport(travel_time)
+                else:
+                    continue         
+                possibilities.append(transport)    
+            return(possibilities)
+        except IndexError:
+            print("Problem with the origin or destination address (not found)")
+        except requests.exceptions.ConnectionError:
+            print("Are you in Bouygues? Because you have no internet connection. Go out and try again")  
+
 
 
                 
