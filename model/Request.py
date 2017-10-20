3df0ebd3db2bdac45a76012e9f1e3c46e22c3d7e
Robin <robin.multon@hotmail.fr>
2017-10-20 11:54:13 +0200

    Changes Request object to ease type verification

--- model/Request.py
+++ model/Request.py
@@ -1,63 +1,51 @@
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def check_float(name, string):
+    if not is_float(string):
+        raise InvalidRequestError
+        return False
+    else:
+        return True
+
 class Request():
 
-    def __init__(self,request_from,request_to):
-        self._request_from = self.check_request_from(request_from)
-        self._request_to = self.check_request_to(request_to)
+    def __init__(self, from_x, from_y, to_x, to_y):
+        self._from_x = from_x
+        self._from_y = from_y
+        self._to_x = to_x
+        self._to_y = to_y
+        self.check_request()
+    
+    def check_request(self):
+        for field_name, field_value in self.__dict__.items():
+            check_float(field_name, field_value)
+        
+    @property
+    def from_x(self):
+        return self._from_x
 
+    @property
+    def from_y(self):
+        return self._from_y
 
+    @property
+    def to_x(self):
+        return self._to_x
 
-    """ check methods for _request_from and _request_to attributes """
+    @property
+    def to_y(self):
+        return self._to_y
 
-
-    def check_request_from(self, request_from):
-        try:
-            request_from[0] = float(request_from[0])
-            request_from[1] = float(request_from[1])
-            assert request_from[0] > 48.818343 and request_from[0] < 48.905555 and request_from[1] > 2.240706 and request_from[1] < 2.412400
-
-        except AssertionError:
-            print("The origin address you typed is not in Paris")
-            return "unvalid"
-        except ValueError:
-            print("You didn't type a valid origin address (not convertible in float)")
-            return "unvalid"
-        else :
-            return request_from
-
-    def check_request_to(self, request_to):
-        try:
-            request_to[0] = float(request_to[0])
-            request_to[1] = float(request_to[1])
-            assert request_to[0] > 48.818343 and request_to[0] < 48.905555 and request_to[1] > 2.240706 and request_to[1] < 2.412400
-        except AssertionError:
-            print("The destination address you typed is not in Paris")
-            return "unvalid"
-        except ValueError:
-            print("You didn't type a valid destination address (not convertible in float)")
-            return "unvalid"
-        else :
-            return request_to
-
-    """ getters and setters de _request_from et _request_to """
-
-    def _get_request_from(self):
-        return self._request_from
-
-    def _set_request_from(self,request_from):
-        print("Don't change the f*cking adress, yo")
-
-
-    request_from=property(_get_request_from,_set_request_from)
-
-
-
-    def _get_request_to(self):
-        return self._request_to
-
-    def _set_request_to(self,request_to):
-        print("Don't change the f*cking adress, yo")
-
-    request_to=property(_get_request_to,_set_request_to)
-
-
-
+class InvalidRequestError(Exception):
+    def __init__(self, value, field_name):
+        self.value = value
+        self.field_name = field_name
+    def __repr__(self):
+        return '{} is {} : not convertible to float'.format(self.field_name, self.value)
+    def __str__(self):
+        return '{} is {} : not convertible to float'.format(self.field_name, self.value)
