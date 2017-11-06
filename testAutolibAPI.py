1da141897902c1ec850974d38b31441963f6e091
Robin <robin.multon@hotmail.fr>
2017-11-06 13:55:43 +0100

    Removed useless files

--- testAutolibAPI.py
+++
@@ -1,7 +0,0 @@
-from model.Request import Request
-from webservice_caller.AutolibAPICaller import AutolibAPI
-
-request = Request('48.846559','2.344506','48.8462006','2.2763742')
-caller = AutolibAPI(request)
-res = caller.get_time()
-print(res)
