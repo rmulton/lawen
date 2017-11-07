faf34fff3ce74d2f3725664672c952898c47cc03
Robin <robin.multon@hotmail.fr>
2017-11-07 15:46:41 +0100

    Added new error handling

--- webservice_caller/call_url.py
+++ webservice_caller/call_url.py
@@ -5,6 +5,8 @@ def call_url(url):
         return requests.get(url)
     except requests.exceptions.RequestException as e:
         raise APICallError
+    except requests.exceptions.ConnectionError as e:
+        raise APICallError
 
 class APICallError(Exception):
     def __init__(self):
